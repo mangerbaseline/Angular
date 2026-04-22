89512498ac079a46c4a8bfb4c3fb4720adcaadc9
sajan <sajansajan85611@gmail.com>
2026-04-22 15:28:37 +0530

    new udpated code  22 april 2026

--- src/app/components/payment-card/payment-card.component.ts
+++ src/app/components/payment-card/payment-card.component.ts
@@ -654,7 +654,7 @@ export class PaymentCardComponent implements OnInit, AfterViewInit {
     });
 
     try {
-      this.stripe = Stripe("pk_test_51Q0xUnBrVsb68zkxVIQXjAHQqONjjk6jyFoE9HQ7zIn44MszuDGs6QT97k6QKQhNUfs7b54dVTV6A6tumWvD3nU200nU1Q1Yel");
+      this.stripe = Stripe("pk_live_51Q0xUnBrVsb68zkxIAj8YuXcN6nXJlyUKJyNHdek85ZZ6qrFiEC293Qz83hnvmwxV1tySjDFNile7X83yoQq5NNC00GbCsdGNJ");
     } catch (e) {
       console.error("Stripe.js not loaded", e);
     }
